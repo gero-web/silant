a36868e6d5c22935c1ccf5add9ec9cd8ddea2661
Local Admin <sergeq192.gmail.com>
2023-07-07 16:41:11 +0800

    fix: mexin and tags has-groups

--- silant/tags/has_group.py
+++ silant/tags/has_group.py
@@ -5,4 +5,4 @@ register = template.Library()
 @register.filter(name='has_group')
 def has_group(user, groups):
     groups = groups.split()
-    return user.groups.filter(name__icontains=groups)
+    return user.groups.filter(name__in=groups).exists()
--- silant/utils/check_group.py
+++ silant/utils/check_group.py
@@ -4,7 +4,9 @@ from django.core.exceptions import PermissionDenied
 class CheckPremGroupMexin:
     group = []
     def dispatch(self, request, *args, **kwargs):
+        print('trfdsfrdsajmfkds')
         if request.user.groups.filter(name__in = self.group):
-            return True
+           
+           return super().dispatch(request, *args, **kwargs)
         else:
             raise PermissionDenied(' У вас нет прав для выполнения данного запроса',)
